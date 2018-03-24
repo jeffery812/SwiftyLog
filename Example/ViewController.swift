0a27ebf73afe89fba555b487c0d23a937d2e79c5
Zhihui Tang <crafttang@gmail.com>
2018-03-24 08:17:45 +0100

    carthage archive SwiftMagic

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Zhihui Tang on 2018-03-23.
+//  Copyright © 2018 Zhihui Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
